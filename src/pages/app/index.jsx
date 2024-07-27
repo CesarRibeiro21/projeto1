fd9fd84ff876e9d0c15a4e87f78f6e3fa418f5a4
Cesar Ribeiro <cesartertulianort@gmail.com>
2024-07-27 20:50:27 -0300

    Atualizando

---
+++ src/pages/app/index.jsx
@@ -0,0 +1,14 @@
+
+import './index.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Bora estudar ReactJs!!!
+        <i className='fa fa-heart'></i>
+      </h1>
+    </div>
+  );
+}
+
+
