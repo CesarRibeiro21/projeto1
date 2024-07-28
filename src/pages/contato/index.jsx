f9012081a9f92b21fcba044789b82e4a4ef59150
Cesar Ribeiro <cesartertulianort@gmail.com>
2024-07-28 12:20:56 -0300

    nova página

---
+++ src/pages/contato/index.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+export default function Contato(){
+
+    return (
+        <div className='pagina-contato'>
+            <h1>Contato</h1>
+            <img className='icone' src='/assets/images/imagem-contato.webp' />
+        </div>
+    )
+}
